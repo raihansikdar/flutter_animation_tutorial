a14428186f6fbb7ef62e27e404a66664a68dfc03
raihan sikdar <raihansikdar10@gmail.com>
2024-10-25 20:49:35 +0600

    animated container

---
+++ android/app/src/main/kotlin/com/example/flutter_animation_tutorial/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_animation_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/animated_container_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AnimatedContainerScreen extends StatefulWidget {
+  const AnimatedContainerScreen({super.key});
+
+  @override
+  State<AnimatedContainerScreen> createState() => _AnimatedContainerScreenState();
+}
+
+class _AnimatedContainerScreenState extends State<AnimatedContainerScreen> {
+
+  double boxHeight = 150;
+  double boxWidth = 150;
+  bool isTap = false;
+  Color color = Colors.red;
+  void startAnimation(){
+    setState(() {
+      isTap = !isTap;
+      boxHeight = isTap ? 300 : 150;
+      boxWidth = isTap ? 300: 150;
+      color = isTap ? Colors.blue : Colors.red;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Animated Container"),
+        backgroundColor: Colors.grey.shade300,
+      ),
+     body: Center(
+       child: GestureDetector(
+         onTap: (){
+           startAnimation();
+         },
+         child: AnimatedContainer(
+           duration: Duration(seconds: 1),
+           height: boxHeight,
+           width: boxWidth,
+           color: color,
+           curve: Curves.bounceOut,
+         ),
+       ),
+     ),
+    );
+  }
+}
